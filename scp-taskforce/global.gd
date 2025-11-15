0c28ac7bed7d4393ed8d55fd6f0fa5eef26057e1
Kadsper23 <kadensperry7@gmail.com>
2025-11-14 17:52:50 -0700

    dialouge start

---
+++ scp-taskforce/global.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var alarm: AudioStreamPlayer = %alarm
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if get_tree().current_scene.name == "startscene":
+		DialogueManager.show_example_dialogue_balloon(load("res://testuntitled.dialogue"))
+
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
