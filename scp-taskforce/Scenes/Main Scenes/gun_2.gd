5ec9125b6c900e079a919627c2982eee9f563874
Kadsper23 <kadensperry7@gmail.com>
2025-11-15 00:42:29 -0700

    level1 floor

--- scp-taskforce/Scenes/Main Scenes/gun_2.gd
+++ scp-taskforce/Scenes/Main Scenes/gun_2.gd
@@ -1,4 +1,5 @@
 extends CollisionShape2D
+@onready var player: CharacterBody2D = $"../../Player"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +9,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if $RayCast2D.is_colliding():
+		print("coliding")
